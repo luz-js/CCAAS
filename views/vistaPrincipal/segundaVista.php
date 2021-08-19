e567c6c65f4ecdb559e9483f17463546314047e5
luz mariel rosario garcia <luri1308@hotmail.com>
2021-08-19 07:20:47 -0500

    segunda vista de inicio

--- views/vistaPrincipal/segundaVista.php
+++ views/vistaPrincipal/segundaVista.php
@@ -1,4 +1,23 @@
 <div class="segunda-vista">
-    <h1> <span></span> </h1>
+    <h1> NUESTROS PASTORES </h1>
+    <div class="segundaVista-container">
+        <div class="segundaVista-left">
+            <h2><span>Padres</span> Espirituales</h2>
+            <div class="raya-pastores"></div>
+            <p class="textoPastores">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, <br /> <br>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. More recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            <div class="segundaVista-btn">
+                <a href="" class="a1"> Ver visión</a>
+                <a href="" class="a2"> Casas de paz</a>
+            </div>
+        </div>
+        <div class="segundaVista-raya">
+            
+        </div>
+        <div class="segundaVista-right">
+            <div class="img-pastores"></div>
+        </div>
+    </div>
 </div>
 
